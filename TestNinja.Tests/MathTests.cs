e3029ee23038464f69cdacabe6d9a74d2ad1c41e
Anoop Simon <anoop.simon@adudev.com>
2020-05-13 17:08:48 +1000

    initial comit

---
+++ TestNinja.Tests/MathTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+using Math = TestNinja.Fundamentals.Math;
+
+namespace TestNinja.Tests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        private Math _math;
+
+        [SetUp]
+        public void Setup() {
+             _math = new Math();
+        }
+
+        [Test]
+        [Ignore("This requirement will change in release 3")]
+        public void Math_Add_WhenCalled_ReturnSumOfArgument()
+        {
+
+            var result= _math.Add(10, 10);
+
+            Assert.That(result ,Is.EqualTo(20));
+
+        }
+
+        [Test]
+        [TestCase(2,1,2)]
+        [TestCase(1, 2,2)]
+        [TestCase(2, 2,2)]
+        public void Math_Max_FirstArgumentIsGreater_ReturnMaxArgument(int argument1 , int argument2,int expected) {
+
+            var result = _math.Max(argument1, argument2);
+
+            Assert.That(result, Is.EqualTo(expected));
+
+        }
+
+       
+    }
+}
---
+++ TestNinja.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinja.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestNinja.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cec77e18-c596-4498-8aa8-b5fe53f81816")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
